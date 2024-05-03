6ebec282398b0c2d47dc236ddf6a6aa4b74dd917
Sithiphone <sithiphone@fe-nuol.edu.la>
2024-05-03 16:51:11 +0700

    add app.js to github

---
+++ app.js
@@ -0,0 +1,12 @@
+// console.log("Hello, " + process.argv[2] + "!");
+/* var firstName = 'Sitthiphone';
+var lastName = 'PHANDALA';
+
+console.log(firstName + " " + lastName); 
+let className = "API Development by NodeJS";
+console.log(className); 
+*/
+const emp_id = 110;
+console.log("Employee ID: " + emp_id);
+emp_id = 111;
+console.log("Employee ID: " + emp_id);
