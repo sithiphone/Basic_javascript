1f20d02d970fa9bdd5de8622b25bcee2ba8de9bd
Sithiphone <sithiphone@fe-nuol.edu.la>
2024-05-06 15:51:25 +0700

    update code in variable.js

--- variable.js
+++ variable.js
@@ -1,6 +1,36 @@
+//Number type
 let age = 18;
 console.log(age); // 18
 let blue = 0x0000ff;
 console.log(blue); // 255
 let pi = 3.14159;
 console.log(pi); // 3.14159
+
+//String type
+let firstName = 'Sithiphone';
+let lastName = "PHANDALA";
+console.log(firstName + " " + lastName); // John Doe
+
+//Boolean type
+let isReading = true;
+let isSleeping = false;
+console.log(isReading); // true
+console.log(isSleeping); // false
+
+//Null type
+let car = null;
+console.log(car); // null
+
+//Undefined type
+let x;
+console.log(x); // undefined
+
+//Symbol type
+let sym1 = Symbol('foo');
+console.log(sym1); // Symbol(foo)
+
+//Object type
+let myCar = new Object();
+myCar.make = 'Ford';
+myCar.model = 'Mustang';
+console.log(myCar.make); // Ford
